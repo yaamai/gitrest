1bc2172f3cf500788a595325aeed82e3bf7ed0a7
yaamai <ya_amai@yahoo.co.jp>
2021-03-20 21:13:13 +0900

    bump

--- main.go
+++ main.go
@@ -1,103 +1,159 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"os"
 
 	"github.com/go-git/go-billy/v5/memfs"
 	"github.com/go-git/go-git/v5"
-	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/object"
 	"github.com/go-git/go-git/v5/storage/memory"
+	"github.com/gorilla/mux"
 )
+
+func initServer(repo *git.Repository) {
+	worktree, err := repo.Worktree()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/{path}", func(w http.ResponseWriter, r *http.Request) {
+		f, err := worktree.Filesystem.Open(r.URL.Path)
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+		io.Copy(w, f)
+	}).Methods("GET")
+
+	r.HandleFunc("/{path}", func(w http.ResponseWriter, r *http.Request) {
+		p := mux.Vars(r)["path"]
+		f, err := worktree.Filesystem.Create(p)
+		if err != nil {
+			http.NotFound(w, r)
+			return
+		}
+		io.Copy(f, r.Body)
+
+		worktree.AddWithOptions(&git.AddOptions{Path: p})
+		_, err = worktree.Commit("test", &git.CommitOptions{})
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}).Methods("POST")
+
+	r.HandleFunc("/commits/{path}", func(w http.ResponseWriter, r *http.Request) {
+		p := mux.Vars(r)["path"]
+		commitIter, _ := repo.Log(&git.LogOptions{FileName: &p})
+		commitIter.ForEach(func(c *object.Commit) error {
+			log.Println(c.Hash)
+			return nil
+		})
+	})
+
+	wd, _ := os.Getwd()
+	log.Println(wd)
+	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("."))))
+
+	http.ListenAndServe(":13223", r)
+}
 
 func main() {
 
 	fs := memfs.New()
-	r, err := git.Init(memory.NewStorage(), fs)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Println("repo:", r)
-
-	w, err := r.Worktree()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Println("worktree", w)
-
-	f, err := fs.Create("aa")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	f.Write([]byte("test"))
-	f.Close()
-
-	w.AddWithOptions(&git.AddOptions{Path: "aa"})
-	_, err = w.Commit("test", &git.CommitOptions{})
+	repo, err := git.Init(memory.NewStorage(), fs)
 	if err != nil {
 		log.Fatalln(err)
 	}
 
-	ref, err := r.Head()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Println(ref)
+	initServer(repo)
 
-	commit, err := r.CommitObject(ref.Hash())
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Println("commit:", commit)
+	/*
+		log.Println("repo:", r)
 
-	obj, err := r.Storer.EncodedObject(plumbing.CommitObject, commit.Hash)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	objr, err := obj.Reader()
-	if err != nil {
-		log.Fatalln(err)
-	}
+		w, err := r.Worktree()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Println("worktree", w)
 
-	buf := make([]byte, 1024)
-	l, err := objr.Read(buf)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Println("commit-obj:", buf[:l], string(buf[:l]))
-	log.Println("commit-encode:", obj)
+		f, err := fs.Create("aa")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		f.Write([]byte("test"))
+		f.Close()
 
-	iter, err := commit.Files()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	iter.ForEach(func(f *object.File) error {
-		log.Println("file:", f)
-		lines, err := f.Lines()
-		log.Println(lines, err)
-		return nil
-	})
-	// /refs/master/path/to/file
-	// /commits/hash/path/to/file
-	// /tags?branches?/master/path/to/file
+		w.AddWithOptions(&git.AddOptions{Path: "aa"})
+		_, err = w.Commit("test", &git.CommitOptions{})
+		if err != nil {
+			log.Fatalln(err)
+		}
 
-	tree, err := commit.Tree()
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Println("tree:", tree)
+		ref, err := r.Head()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Println(ref)
 
-	file, err := tree.File("aa")
-	if err != nil {
-		log.Fatalln(err)
-	}
-	log.Println("file:", file)
+		commit, err := r.CommitObject(ref.Hash())
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Println("commit:", commit)
 
-	fr, err := file.Reader()
-	fmt.Println(err)
+		obj, err := r.Storer.EncodedObject(plumbing.CommitObject, commit.Hash)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		objr, err := obj.Reader()
+		if err != nil {
+			log.Fatalln(err)
+		}
 
-	l, err = fr.Read(buf)
-	fmt.Println(l, err)
-	log.Println(string(buf[:l]))
+		buf := make([]byte, 1024)
+		l, err := objr.Read(buf)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Println("commit-obj:", buf[:l], string(buf[:l]))
+		log.Println("commit-encode:", obj)
+
+		iter, err := commit.Files()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		iter.ForEach(func(f *object.File) error {
+			log.Println("file:", f)
+			lines, err := f.Lines()
+			log.Println(lines, err)
+			return nil
+		})
+		// /refs/master/path/to/file
+		// /commits/hash/path/to/file
+		// /tags?branches?/master/path/to/file
+
+		tree, err := commit.Tree()
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Println("tree:", tree)
+
+		file, err := tree.File("aa")
+		if err != nil {
+			log.Fatalln(err)
+		}
+		log.Println("file:", file)
+
+		fr, err := file.Reader()
+		fmt.Println(err)
+
+		l, err = fr.Read(buf)
+		fmt.Println(l, err)
+		log.Println(string(buf[:l]))
+	*/
 }
