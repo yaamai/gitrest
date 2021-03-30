b49174e86a36228fd57ba0e357c90fbb19108947
yaamai <ya_amai@yahoo.co.jp>
2021-03-30 13:45:28 +0900

    Test iter

--- main.v
+++ main.v
@@ -1,7 +1,6 @@
 import time
 
 
-
 fn new_id(site u32, cnt u32) u64 {
 	return u64(site) << 32 | u64(cnt)
 }
@@ -11,30 +10,35 @@ struct Op {
 	id u64
 	origin u64
 mut:
-	left &Op
-	right &Op
+	left u64
+	right u64
 	deleted bool
 	content int
 }
 
 // We represent linear data as a doubly linked list S of insertions
 struct OpSet {
-	left &Op
-	right &Op
+	left u64
+	right u64
+	data map[u64]&Op
 }
 
 fn new_opset() OpSet {
 	// delimiter
-	mut left := &Op{id: new_id(0, 0), origin: 0, left: 0, right: 0, deleted: false, content: 0}
-	mut right := &Op{id: new_id(0, 1), origin: 0, left: 0, right: 0, deleted: false, content: 0}
-	left.right = right
-	right.left = left
+	mut left := &Op{id: new_id(0, 1), origin: 0, left: 0, right: 0, deleted: false, content: 0}
+	mut right := &Op{id: new_id(0, 2), origin: 0, left: 0, right: 0, deleted: false, content: 0}
+	left.right = right.id
+	right.left = left.id
 	// println(left)
 	// println(right)
 
+	mut data := map[u64]&Op{}
+	data[left.id] = left
+	data[right.id] = right
 	return OpSet{
-		left: left,
-		right: right
+		left: left.id,
+		right: right.id,
+		data: data
 	}
 }
 
@@ -46,8 +50,8 @@ interface OpMutIterator {
 }
 fn (s OpSet) each(iter OpIterator) {
 	// loop left delimiter to right delimiter
-	mut n := s.left.right
-	for ; n != 0 && n.right != 0; n = n.right {
+	mut n := s.data[s.data[s.left].right]
+	for ; n != 0 && n.right != 0; n = s.data[n.right] {
 		if iter.next(n) {
 			break
 		}
@@ -55,8 +59,8 @@ fn (s OpSet) each(iter OpIterator) {
 }
 fn (s OpSet) each_all(iter OpMutIterator) {
 	// loop left delimiter to right delimiter
-	mut n := s.left
-	for ; n != 0 && n.right != 0; n = n.right {
+	mut n := s.data[s.left]
+	for ; n != 0 && n.right != 0; n = s.data[n.right] {
 		if iter.next(mut n) {
 			break
 		}
@@ -106,33 +110,61 @@ fn (s OpSet) at(pos int) ?&Op {
 struct OpAdd {
 mut:
 	target int
+	op &Op
 	content int
 	id u64
 	pos int
 }
 fn (mut a OpAdd) next(mut op &Op) bool {
 	if a.pos == a.target {
-		new_op := &Op{id: a.id, origin: op.id, left: op, right: op.right, deleted: false, content: a.content}
-		op.right.left = new_op
-		op.right = new_op
+		a.op = &Op{
+			...(*a.op)
+			origin: op.id,
+			left: op.id,
+			right: op.right
+		}
 		return true
 	}
 	a.pos++
 	return false
 }
-fn (mut s OpSet) add(pos int, content int) {
-	add := OpAdd{target: pos, content: content, id: new_id(1, u32(time.now().unix_time()))}
+fn (s OpSet) gen_insert_op(pos int, content int) &Op {
+	add := OpAdd{target: pos, op: &Op{content: content, id: new_id(1, u32(time.now().unix_time()))}}
 	s.each_all(add)
+	return add.op
+}
+
+fn (mut s OpSet) insert(pos int, content int) {
+	op := s.gen_insert_op(pos, content)
+	s.integrate(op)
+}
+
+fn (mut s OpSet) integrate(op &Op) {
+}
+
+
+struct ItTest {}
+
+fn (t &ItTest) next() ?int {
+	return 0
 }
 
 fn main() {
+
+	iter := ItTest{}
+	for i in iter {
+		println(i)
+	}
+
+
+
 	mut s := new_opset()
 	println("empty: ${s}")
-	s.add(0, 1)
+	s.insert(0, 1)
 	println("add: ${s}")
-	s.add(0, 2)
+	s.insert(0, 2)
 	println("add: ${s}")
-	s.add(1, 3)
+	s.insert(1, 3)
 	println("add: ${s}")
 	at0 := s.at(0) or { panic("position 0 not found") }
 	println("at 0: ${at0}")
